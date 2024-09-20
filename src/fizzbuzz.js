3201cbaba6c51f3c17070c741ffcd01962c59c64
agrmet <agron-metaj@hotmail.com>
2024-09-20 14:10:41 +0200

    Finished

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,9 +2,22 @@ const answer = []
 
 // Write your code below this line
 
+for (let index = 1; index <= 15; index++) {
+  const element = fizzBuzz(index)
+  answer.push(element)
+}
 
-
-
+function fizzBuzz(input) {
+  if (input % 5 === 0 && input % 3 === 0) {
+    return 'FizzBuzz'
+  } else if (input % 5 === 0) {
+    return 'Buzz'
+  } else if (input % 3 === 0) {
+    return 'Fizz'
+  } else {
+    return input
+  }
+}
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
